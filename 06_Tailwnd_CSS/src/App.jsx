a26b6368cb54145539de9bb015489cfa5573a0e5
Akash <therealakash13@gmail.com>
2026-01-02 17:13:28 +0530

    Applying tailwind in react

---
+++ 06_Tailwnd_CSS/src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return <div className="w-full h-screen bg-gray-600">
+    <h1 className="text-4xl font-bold text-gray-200 p-5">Welcome to React with Tailwind CSS</h1>
+  </div>;
+}
