00b2ce9f2c3ce24342cc0a09739b56d6cb42a364
Akash <therealakash13@gmail.com>
2026-01-07 18:07:59 +0530

    completed the challenge

--- 07_2_Tailwind_React_Challenge/src/components/Footer/Footer.jsx
+++ 07_2_Tailwind_React_Challenge/src/components/Footer/Footer.jsx
@@ -1,3 +1,75 @@
+import { Call, Facebook, Twitter, Youtube } from "./SVGContent";
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className=" text-white px-6 py-10  bg-[url('https://images.unsplash.com/photo-1617897711385-df9c86b7dfe3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-center bg-cover">
+      <div className="w-full flex mb-4">
+        <div className="w-2/5 flex flex-col gap-3">
+          <p className="py-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam
+            libero eius vel, tempore numquam est voluptas reprehenderit
+            voluptate sequi adipisci enim sit cumque inventore quo ab deserunt
+            eaque sed nihil!
+          </p>
+          <div className="flex gap-4">
+            <Facebook
+              width={40}
+              height={40}
+              fill={"#112b42"}
+              className="cursor-pointer"
+            />
+            <Twitter
+              width={40}
+              height={40}
+              fill={"#112b42"}
+              className="cursor-pointer"
+            />
+            <Youtube
+              width={40}
+              height={40}
+              fill={"#112b42"}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
+        <div className="w-2/6 flex flex-col gap-3 px-10">
+          <h3 className="text-3xl font-bold text-secondary">Navigation</h3>
+          <ul className=" flex flex-col gap-2.5">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Pages</li>
+            <li className="cursor-pointer">About Us</li>
+            <li className="cursor-pointer">Services</li>
+          </ul>
+        </div>
+        <div className="w-2/6 flex flex-col gap-3 px-10">
+          <h3 className="text-3xl font-bold text-secondary">Quick Links</h3>
+          <ul className=" flex flex-col gap-2.5">
+            <li className="cursor-pointer">Contact Us</li>
+            <li className="cursor-pointer">FAQs</li>
+            <li className="cursor-pointer">Blog</li>
+            <li className="cursor-pointer">Gallery</li>
+            <li className="cursor-pointer">Pricing</li>
+          </ul>
+        </div>
+        <div className="w-2/6 flex flex-col gap-3 px-10">
+          <h3 className="text-3xl font-bold text-secondary">Work Hours</h3>
+          <ul className=" flex flex-col gap-2.5">
+            <li>7 A.M. - 5 P.M, Mon - Sat</li>
+            <li>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur,
+              labore.
+            </li>
+            <button className="w-fit py-3 px-6 flex gap-3 rounded-2xl bg-primary">
+              <Call fill={"#fff"} width={20} height={20} />
+              Call Us
+            </button>
+          </ul>
+        </div>
+      </div>
+
+      <div className="text-center w-full border-t-2 border-white flex justify-center items-center pt-6">
+        <span>@copyright 2025</span>
+      </div>
+    </div>
+  );
 }
--- 07_2_Tailwind_React_Challenge/src/components/Sections/Section5.jsx
+++ 07_2_Tailwind_React_Challenge/src/components/Sections/Section5.jsx
@@ -1,8 +1,8 @@
 export default function Section5() {
   return (
-    <div className="w-full py-24 text-white bg-accent">
-      <div className="w-1/2 flex gap-4">
-        <h2>Our Newsletters</h2>
+    <div className="w-full py-24 text-white bg-accent flex my-8">
+      <div className="w-1/2 flex flex-col gap-6 px-8">
+        <h2 className="text-5xl font-semibold">Our Newsletters</h2>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
           veniam eligendi officiis voluptate incidunt sapiente sit deserunt
@@ -10,9 +10,9 @@ export default function Section5() {
           Exercitationem natus esse voluptas.
         </p>
       </div>
-      <div className="w-1/2 flex gap-3">
-        <input type="text" placeholder="Email" />
-        <button className="bg-primary w-fit">Submit</button>
+      <div className="w-1/2 flex gap-8 items-center justify-center">
+        <input className="outline-white border-2 px-3 py-4 rounded-2xl" type="text" placeholder="Email" />
+        <button className="bg-primary w-fit px-6 py-4 rounded-2xl cursor-pointer">Submit</button>
       </div>
     </div>
   );
